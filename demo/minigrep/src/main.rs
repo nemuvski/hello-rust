9e32b0747163ee19a7e0ebed46187698e96ad533
K.Utsunomiya <kii.utsunomiya@gmail.com>
2020-08-30 16:57:57 +0900

    引数のチェック, 抽出を関数化

--- demo/minigrep/src/main.rs
+++ demo/minigrep/src/main.rs
@@ -4,14 +4,7 @@ use std::io::prelude::*;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  if args.len() != 3 {
-    println!("Arguments:");
-    println!("\t1st: Search string");
-    println!("\t2nd: Path to File");
-    panic!();
-  }
-  let query = &args[1];
-  let path_to_file = &args[2];
+  let (query, path_to_file) = parse_arguments(&args);
 
   let mut f = File::open(path_to_file).expect("file not found");
   let mut contents = String::new();
@@ -19,4 +12,15 @@ fn main() {
     .expect("something went wrong reading the file");
 
   println!("With text:\n{}", contents);
+}
+
+fn parse_arguments(args: &Vec<String>) -> (&String, &String) {
+  if args.len() != 3 {
+    println!("Arguments:");
+    println!("\t1st: Search string");
+    println!("\t2nd: Path to File");
+    panic!()
+  } else {
+    (&args[1], &args[2])
+  }
 }
