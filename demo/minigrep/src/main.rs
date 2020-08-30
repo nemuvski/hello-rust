c49968aca1f868616de6c41377f6528e04f4f0c2
K.Utsunomiya <kii.utsunomiya@gmail.com>
2020-08-30 16:40:46 +0900

    ファイル読み込みと内容の出力を実装

--- demo/minigrep/src/main.rs
+++ demo/minigrep/src/main.rs
@@ -1,18 +1,22 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-
   if args.len() != 3 {
     println!("Arguments:");
     println!("\t1st: Search string");
-    println!("\t2nd: File name");
+    println!("\t2nd: Path to File");
     panic!();
   }
-
   let query = &args[1];
-  let filename = &args[2];
+  let path_to_file = &args[2];
 
-  println!("Searching for {}", query);
-  println!("In file {}", filename);
+  let mut f = File::open(path_to_file).expect("file not found");
+  let mut contents = String::new();
+  f.read_to_string(&mut contents)
+    .expect("something went wrong reading the file");
+
+  println!("With text:\n{}", contents);
 }
