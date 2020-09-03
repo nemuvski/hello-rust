fba50946d2abdc1ef1e8aa34b2f3d4ec911804e8
K.Utsunomiya <kii.utsunomiya@gmail.com>
2020-09-03 22:05:37 +0900

    println!→eprintln!に変更 (標準エラー出力)

--- demo/minigrep/src/main.rs
+++ demo/minigrep/src/main.rs
@@ -9,13 +9,13 @@ fn main() {
   let args: Vec<String> = env::args().collect();
   // Note: unwrap_or_elseで,panic!ではなく独自のエラー処理を定義できる.
   let config = Config::new(&args).unwrap_or_else(|err| {
-    println!("{}", err);
+    eprintln!("{}", err);
     process::exit(1);
   });
 
   // Note: runがErr値を返したかどうかだけ気にすれば良いので,if-letで記述している.(成功時は()でしかないため)
   if let Err(err) = minigrep::run(config) {
-    println!("Application error: {}", err);
+    eprintln!("Application error: {}", err);
     process::exit(1);
   }
 }
